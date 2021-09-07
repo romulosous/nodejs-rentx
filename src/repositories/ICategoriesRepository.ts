0268c622298cd52d52f40a1d897524123337261b
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-09-06 23:03:01 -0300

    using liskov substitution principle

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -11,4 +11,4 @@ interface ICategoriesRepository {
   create({ name, description }: ICreateCategoryDTO): void;
 }
 
-export { ICategoriesRepository };
+export { ICategoriesRepository, ICreateCategoryDTO };
