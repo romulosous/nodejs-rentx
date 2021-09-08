87638e0a94515be411ae04274cbcfd03c79f84c1
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-09-08 15:03:36 -0300

    fix import/separating repositories

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,4 +1,5 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
